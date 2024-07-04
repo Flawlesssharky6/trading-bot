4f344983bec6cee2414f325f2bee2b1cbde00bcc
Flawlesssharky6 <Chengqianglin512@gmail.com>
2024-07-04 00:26:11 -0600

    added machine learning model

--- tradingbot.py
+++ tradingbot.py
@@ -5,6 +5,7 @@ from lumibot.traders import Trader # delpoyment capability for running live
 from datetime import datetime
 from alpaca_trade_api import REST
 from timedelta import Timedelta
+from finbert_utils import estimate_sentiment # the machine learning model
 
 API_KEY = "PKFI2YEDDK32ILJ49DK2"
 API_SECRET = "NaPGvSelWmrwnNxTKvEYBVMTckMQzKN6lxIwUP57"
@@ -36,13 +37,14 @@ class MLTrader(Strategy):
         three_days_prior = today - Timedelta(days=3)
         return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
     
-    def get_news(self):
+    def get_sentiment(self):
         today, three_days_prior = self.get_dates()
         news = self.api.get_news(symbol=self.symbol,
                                  start=three_days_prior,
                                  end=today)
         news = [ev.__dict__["_raw"]["headline"] for ev in news]
-        return news
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
         
             
     def on_trading_iteration(self):
@@ -50,8 +52,8 @@ class MLTrader(Strategy):
         
         if cash > last_price:
             if self.last_trade == None:
-                news = self.get_news()
-                print(news)
+                probability, sentiment = self.get_sentiment()
+                print(probability, sentiment)
                 order = self.create_order(
                     self.symbol,
                     quantity,
